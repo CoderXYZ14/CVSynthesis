8b63c7fca9db66fb6502b880d8b1e14aa0adc7da
CoderXYZ14 <s1404islam@gmail.com>
2024-09-26 20:24:29 +0530

    active Form logic in form section

--- src/components-page/resumeComponents/FormSection.jsx
+++ src/components-page/resumeComponents/FormSection.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { PersonalDetail } from "../forms";
+import { ArrowLeft, ArrowRight, LayoutGrid } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 const FormSection = () => {
-  return <div>FormSection</div>;
+  const [activeFormIndex, setActiveFormIndex] = useState(1);
+  return (
+    <div>
+      <div className="flex justify-between items-center">
+        <Button variant="outline" size="sm" className="flex gap-2">
+          <LayoutGrid />
+          Theme
+        </Button>
+        <div className="flex gap-2">
+          {activeFormIndex > 1 && (
+            <Button
+              size="sm"
+              onClick={() => setActiveFormIndex(activeFormIndex - 1)}
+            >
+              <ArrowLeft />
+            </Button>
+          )}
+          <Button
+            className="flex gap-2"
+            size="sm"
+            onClick={() => setActiveFormIndex(activeFormIndex + 1)}
+          >
+            Next <ArrowRight />
+          </Button>
+        </div>
+      </div>
+      {/* Personal Detail */}
+      {activeFormIndex === 1 && <PersonalDetail />}
+      {/* sUMMARY */}
+      {/* eXPERIECNE */}
+      {/* Educational Detail */}
+      {/* Skills */}
+    </div>
+  );
 };
 
 export default FormSection;
